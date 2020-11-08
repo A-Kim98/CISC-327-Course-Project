c59c436c57c0bb2a65e080fce64e5a0498ac6358
Alice Kim <71195524+A-Kim98@users.noreply.github.com>
2020-11-08 18:46:44 -0500

    Update qa327/frontend.py
    Co-authored-by: lucidorangee <44420769+lucidorangee@users.noreply.github.com>

--- qa327/frontend.py
+++ qa327/frontend.py
@@ -110,7 +110,7 @@ def login_post():
         
     # Password has to meet the required complexity: minimum length 6, at least one upper case, at least one lower case, and at least one special character
     if len(password) < 6:
-        return render_template('login.html', message="Password needs minimum length 6")
+        return render_template('login.html', message="Password needs minimum length 6.")
     
     # at least one upper case, at least one lower case, and at least one special character
     if len(password) > 6:
