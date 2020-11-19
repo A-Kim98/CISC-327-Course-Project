6bf6a5593d5fdcd44a7f2d6b1777facb37344b08
alice_kim <alicekimmy2020@gmail.com>
2020-11-19 17:07:34 -0500

    Completed R2 test cases

--- qa327/frontend.py
+++ qa327/frontend.py
@@ -2,6 +2,7 @@ from flask import render_template, request, session, redirect
 from qa327 import app
 import qa327.backend as bn
 import re #regular expressions
+import string
 
 """
 This file defines the front-end part of the service.
@@ -20,8 +21,9 @@ Validate email complexity and possible email format errors
 # Email, password, password2 all have to satisfy the same required as defined in R1
 # Email and password both cannot be empty
 # Email has to follow addr-spec defined in RFC 5322
+# If the email already exists, show message 'this email has been ALREADY used'
 '''
-def validate_email(email, error_message):
+def validate_email(email, error_message, user):
     email_regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
     if len(email) <= 1:
         error_message = "Email and/or password cannot be empty."
@@ -33,6 +35,9 @@ def validate_email(email, error_message):
         elif len(email) > 30:
             error_message = "Email/password format is incorrect."
             
+        elif user:
+            error_message = "This email has been ALREADY used."
+
         else:
             error_message = ""
     return error_message
@@ -48,17 +53,15 @@ def validate_password(password, password2, error_message):
         error_message = "Email and/or password cannot be empty."
         
     elif (len(password) < 6 or len(password2) < 6) or (len(password) < 6 and len(password2) < 6):
-        error_message = "Password needs minimum length 6"
+        error_message = "Password has to meet the required complexity: minimum length 6, at least one upper case, at least one lower case, and at least one special character."
     
     else:
         if (not re.search(password_check, password)) and  password == password2:
-            error_message = "Password has to meet the required complexity: minimum length 6, at least one upper case, at least one lower case, and at least one special character."
-        elif password != password2:
-            error_message = "Password and password2 have to be exactly the same."
-        elif re.match(r'^\w+$', password):
-            error_message = "Email/password format is incorrect. HERE!"
-        elif re.match(r'^\w+$', password2):
-            error_message = "Email/password format is incorrect. HELLO!"
+               error_message = "Password has to meet the required complexity: minimum length 6, at least one upper case, at least one lower case, and at least one special character."
+
+        elif (not re.search(password_check, password)) or (not re.search(password_check, password2)):
+            error_message = "Password has to meet the required complexity: minimum length 6, at least one upper case, at least one lower case, at least one special character and password and password2 have to be exactly the same."
+            
         else:
           error_message = ""
     return error_message
@@ -69,6 +72,8 @@ Validate username complexity and possible username format error
 # User name has to be non-empty, alphanumeric-only, and space allowed only if it is not the first or the last character.
 '''
 def validate_username(name, error_message):
+    invalidChars = set(string.punctuation.replace("_", ""))
+    
     if len(name) <= 1:
         error_message = "User name has to be non-empty."
         
@@ -79,8 +84,8 @@ def validate_username(name, error_message):
             error_message = "Space allowed only if it is not the first or the last character."
         elif len(name) <= 2 or len(name) >= 20:
             error_message = "User name has to be longer than 2 characters and less than 20 characters."
-        elif (not re.match(r'^\w+$', name)):
-            error_message = "Name format is incorrect."
+        elif any(x in invalidChars for x in name):
+            error_message = "Incorrect Name format!"
         else:
             error_message = ""
     return error_message
@@ -92,12 +97,11 @@ def register_post():
     name = request.form.get('name')
     password = request.form.get('password')
     password2 = request.form.get('password2')
+    user = bn.get_user(email)
     error_message = None
     
-
     # validate email
-    check_email = validate_email(email, error_message)
-    user = bn.get_user(email)
+    check_email = validate_email(email, error_message, user)
     
     # validate password and password2
     check_pwd = validate_password(password, password2, error_message)
@@ -105,32 +109,33 @@ def register_post():
     # validate username
     check_name = validate_username(name, error_message)
     
-    # validate user informations
-    # if there is any error messages when registering new user
-    # at the backend, go back to the register page.
+    '''
+    validate user informations
+        #if there is any error messages when registering new user
+        # at the backend, go back to the register page.
+    '''
+    #email
     if check_pwd == "" and check_name == "":
         if check_email != "":
             return render_template('register.html', message=check_email)
+            
+    if check_pwd == "" or check_name == "":
+        if check_email != "":
+            return render_template('register.html', message=check_email)
+
+    #password
     if check_email == "" and check_name == "":
         if check_pwd != "":
             return render_template('register.html', message=check_pwd)
+    
+    #name
     if check_email == "" and check_pwd == "":
         if check_name != "":
             return render_template('register.html', message=check_name)
     
-    if check_pwd == "" or check_name == "":
-        if check_email != "":
-            return render_template('register.html', message=check_email)
-    
-   
-    # If the email already exists, show message 'this email has been ALREADY used'
-    if user:
-        error_message = "this email has been ALREADY used"
-        return render_template('register.html', message=error_message)
-    
+    #fail to store
     if not bn.register_user(email, name, password, password2):
-        error_message = "Failed to store user info."
-        return render_template('register.html', message=error_message)
+            error_message = "Failed to store user info."
 
     #If no error regarding the inputs following the rules above, create a new user, set the balance to 5000, and go back to the /login page
     if check_email == "" and check_pwd == "" and check_name == "":
